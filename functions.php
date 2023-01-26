3a70eacce0f52ee75bad0a1fcb0c53b1193af1c8
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-01-26 11:26:18 -0500

    s1c2 ajout de add_actions pour enfiles les thems dans funstions.php

--- functions.php
+++ functions.php
@@ -6,9 +6,15 @@
 ?>
 
 <?php 
+
+function enfiler_css() {
 wp_enqueue_style('4w4-gr1-principal', // identificateur
                     get_template_directory_uri() . '/styles.css', // adresse url de style.css
                     array(), //defenir les dépendances
                     filemtime(get_template_directory(). '/style.css'), // le calcul de la version du fichier
                     'all'); //meida
+}
+
+add_action('wp_enqueue_scripts', 'enfiler_css');
+
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mon 4w4</title>
-</head>
-<body>
+<?php /**
+ *  index.php est le model par défaut deu thème 4w4
+ *  */ ?>
+
+<?php get_header(); ?>
+
+<main>
     <h1>bienvenue sur 4w4</h1>
-    <?php wp_head() ?>
+</main>
+    <?php get_footer(); ?>
 </body>
 </html>
