3cc15c2974212843468ed1c0c747b98956318b33
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-03-30 11:02:37 -0400

    test pour les articles pour les mettres en row

--- 404.php
+++ 404.php
@@ -7,6 +7,7 @@
 
 <?php get_header(); ?>
     <main class="error" >
+        <body class="no__aside">
     
         <h1>Erreur 404</h1>
         <h3>Désoler, nous ne pouvons pas trouvé la page que vous chercher.
@@ -15,14 +16,14 @@
     <?php get_search_form()?>
 
     <h3>voici nos choix de cours</h3>
-  <article> <h4><?php  $menu = "cours";
+   <h4><?php  $menu = "cours";
 if(in_category('cours')){$menu = "note-4w4";}
 
 wp_nav_menu(array(
     "menu" => $menu,
     "container" => "nav" 
 )) ?></h4>
-</article>
+
     <h3>voici les notes de cours</h3>
   <article> <h4><?php 
 
@@ -36,7 +37,7 @@ wp_nav_menu(array(
     "container" => "nav" 
 )) ?></h4>
   </article>
-
+  
     </main>
     </body>
     <?php get_footer(); ?>
