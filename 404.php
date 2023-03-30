09a75a070f849bb02e4f2db4e13f14b15f1bd838
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-03-30 09:48:18 -0400

    ajout des cours et notes dans la page

--- 404.php
+++ 404.php
@@ -5,12 +5,20 @@
 ?>
 
 <?php get_header(); ?>
+<body class="site <?php echo(is_404() ? "no-aside" : "")?>">
     <main class="site__main" >
+    
         <h1>Erreur 404</h1>
         <h3>Désoler, nous ne pouvons pas trouvé la page que vous chercher. voulez vous
             essayer une recherche?</h3>
             
-                <?php get_search_form()?>
-           
+    <?php get_search_form()?>
+
+    <h3>voici nos choix de cours</h3>
+    <?php get_template_part("template-parts/categorie", "note-4w4"); ?>
+    <h3>voici les notes de cours</h3>
+    <?php get_template_part("template-parts/categorie", "cours"); ?>
+    
     </main>
+    </body>
     <?php get_footer(); ?>
