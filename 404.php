15348381bf40b831bb66dd1b8a2608ee0f8b387d
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-03-30 10:40:02 -0400

    ajoutdes articles dans les zones approprié

--- 404.php
+++ 404.php
@@ -2,23 +2,41 @@
 /**
  * index.php est le modèle par défaut du thème 4w4 
  */
+
 ?>
 
 <?php get_header(); ?>
-<body class="site <?php echo(is_404() ? "no-aside" : "")?>">
     <main class="error" >
     
         <h1>Erreur 404</h1>
-        <h3>Désoler, nous ne pouvons pas trouvé la page que vous chercher. voulez vous
+        <h3>Désoler, nous ne pouvons pas trouvé la page que vous chercher.
             essayer une recherche?</h3>
             
     <?php get_search_form()?>
 
     <h3>voici nos choix de cours</h3>
-    <?php get_template_part("template-parts/categorie", "note-4w4"); ?>
+  <article> <h4><?php  $menu = "cours";
+if(in_category('cours')){$menu = "note-4w4";}
+
+wp_nav_menu(array(
+    "menu" => $menu,
+    "container" => "nav" 
+)) ?></h4>
+</article>
     <h3>voici les notes de cours</h3>
-    <?php get_template_part("template-parts/categorie", "cours"); ?>
+  <article> <h4><?php 
+
     
+  
+$menu = "note-4w4";
+if(in_category('cours')){$menu = "cours";}
+
+wp_nav_menu(array(
+    "menu" => $menu,
+    "container" => "nav" 
+)) ?></h4>
+  </article>
+
     </main>
     </body>
     <?php get_footer(); ?>
