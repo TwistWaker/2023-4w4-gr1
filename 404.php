fcb5eb38585cbb2ae92f6a01d324aa40665c8e61
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-05-18 11:44:36 -0400

    modification de la page 404

--- 404.php
+++ 404.php
@@ -7,5 +7,6 @@
 <?php get_header(); ?>
     <main class="site__main" >
         <h1>Erreur 404</h1>
+        <h3>veuiller ressayer le lien ou la page désirer</h3>
     </main>
     <?php get_footer(); ?>
