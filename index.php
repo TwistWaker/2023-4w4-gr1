1e398a07b32c11640b57dac7e3e5f9216e2f95aa
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-05-18 11:51:42 -0400

    amélioration de l'index

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 
 
     <?php 
-    // ajouter des post
+    // ajouter des post.
     if(have_posts()):
         while(have_posts()):
             the_post(); // extrait un objet <<post>>?>
             <article>
-           <h3> <?php the_title();// affiche le titre du post ?></h3>
+           <h3> <?php the_title();// affiche le titre du post. ?></h3>
            <h6> Extrait : </h6> <?php the_excerpt(); ?> 
            <h6> Le contenu : </h6> <?php the_content(); ?>
            </article>
