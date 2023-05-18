6fdc482cd2d3b2ed30a22f1640863ad291e94916
Benoit <e1950888@cmaisonneuve.qc.ca>
2023-05-18 11:53:33 -0400

    nettoyage dans la ctegory et le style

--- category.php
+++ category.php
@@ -7,23 +7,7 @@
  *  */ ?>
 
 <?php get_header(); ?>
-<?php /* ?>
-<main>
-    <pre>category.php</pre>
-    <h1>bienvenue sur 4w4</h1>
-    <section class="blocflex">
-<?php if(have_posts()):
-        while(have_posts()): the_post(); ?>
-        <article>
-  <h3>  <a href="<?php the_permalink();?>"> <?php the_title(); ?></a></h3>
-            <hr>
-        <?= wp_trim_words(get_the_excerpt(), 10, " ... ") ?>
-        </article>
-        <?php endwhile; ?>
-       <?php endif;?>
-</section>
-</main>
-<?php */ ?>
+
 
 
 <main class="site__main">
